3a185bc9b620ca8b08924c562d1c37e2cf02ffc8
Verity Davis <toastnjamz@users.noreply.github.com>
2019-01-02 22:02:14 -0800

    Create DirectionModel.cs

---
+++ DirectionModel.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace DungeonAdventure
+{
+    //TODO not sure yet how to use this with the coordinate system for the rooms...
+    public enum Direction : int
+    {
+        North = 0,
+        // NorthEast = 1,
+        East = 1,
+        // SouthEast = 3,
+        South = 2,
+        // SouthWest = 5,
+        West = 3,
+        // NorthWest = 7
+    }
+}
