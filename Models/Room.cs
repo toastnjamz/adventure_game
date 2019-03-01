d621522497f1b1187d1767cd8b9cedfb2d2441ee
toastnjamz <veritydavis@gmail.com>
2019-02-28 19:15:05 -0800

    Adding enemy encounter

--- Models/Room.cs
+++ Models/Room.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventureGame
 {
@@ -15,9 +16,7 @@ namespace AdventureGame
         public string Description { get; private set; }
         public Item ItemRequiredToEnter { get; private set; }
         public List<Item> RoomInventory { get; private set; }
-
-        // TODO public List<EnemyEncounter> EnemiesHere { get; } =
-        // TODO new List<EnemyEncounter>();
+        public List<EnemyEncounter> EnemiesHere { get; private set; }
 
         public Room(int xCoordinate, int yCoordinate, string name, string description, 
             Item itemRequiredToEnter = null, params Item[] RoomItems)
@@ -31,6 +30,7 @@ namespace AdventureGame
             {
                 ItemRequiredToEnter = itemRequiredToEnter;
             }
+            EnemiesHere = new List<EnemyEncounter>();
         }
 
         public void AddItemToRoomInventory(Item item)
@@ -41,6 +41,39 @@ namespace AdventureGame
         public void RemoveItemFromRoomInventory(Item item)
         {
             RoomInventory.Remove(item);
+        }
+
+        // Populates the EnemyEncounter List (list of enemy encounter objects for a room)
+        public void AddEnemy(int enemyID, int chanceOfEncountering)
+        {
+            EnemiesHere.Add(new EnemyEncounter(enemyID, chanceOfEncountering));
+        }
+
+        // Determines which enemy the room has and creates a new instance of the enemy
+        // If there are no enemies in the EnemyEncounter list, return null
+        // Else add all ChanceOfEncountering and get a random number between 1 and ChanceOfEncountering
+        // Then loop through EnemyEncounter objects to find the enenmy
+        public Enemy GetEnemy()
+        {
+            if (!EnemiesHere.Any())
+            {
+                return null;
+            }
+
+            int totalChances = EnemiesHere.Sum(e => e.ChanceOfEncountering);
+            int randomNumber = RandomNumberGenerator.NumberBetween(1, totalChances);
+            int total = 0;
+
+            foreach(EnemyEncounter enemyEncounter in EnemiesHere)
+            {
+                total += enemyEncounter.ChanceOfEncountering;
+
+                if (randomNumber <= total)
+                {
+                    return EnemyFactory.GetEnemy(enemyEncounter.EnemyID);
+                }
+            }
+            throw new ArgumentException("The enemy in this room is on a coffee break, come back later.");
         }
     }
 }
