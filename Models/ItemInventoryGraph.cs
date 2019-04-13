4ff9b352302c8a47166b56df26c43ad01eb0401f
toastnjamz <veritydavis@gmail.com>
2019-04-12 18:48:10 -0700

    adding new graph code

--- Models/ItemInventoryGraph.cs
+++ Models/ItemInventoryGraph.cs
@@ -4,137 +4,129 @@ using System.Collections.Generic;
 namespace AdventureGame
 {
     /// <summary>
-    /// A weighted edge undirected graph of player inventory items.
+    /// A weighted edge undirected graph of all game items (that derive from Item class).
     /// Items are related by edges, and value of pairing is indicated by weight.
     /// Using an adjacency-list representation of relationships between items. 
+    /// Nodes should be the number (count) of items in _itemsMasterList.
     /// </summary>
 
-    public class ItemInventoryGraph
+    public class Test
     {
-        public class Vertex
+        public void DoStuff()
         {
-            public int Index { get; set; }
-            public int ItemID { get; private set; }
-            public string ItemName { get; private set; }
-            public List<Vertex> Neighbors { get; private set; }
-
-            public Vertex(int itemID, string itemName)
+            Graph<Item> graph = new Graph<Item>();
+            if (10 == graph.Head.Value.ID)
             {
-                ItemID = itemID;
-                ItemName = itemName;
-                Neighbors = new List<Vertex>();
+                //Do stuff
+            }
+        }
+    }
+
+    public class Graph<DataType> // where DataType : Item
+    {
+        public class Node
+        {
+            public DataType Value { get; }
+            public List<Edge> Edges { get; }
+
+            public Node(DataType value)
+            {
+                Value = value;
+                Edges = new List<Edge>();
+            }
+
+            public Edge Connect(Node node)
+            {
+                if (null == node)
+                    return null;
+                for (int i = 0; i < Edges.Count; ++i)
+                {
+                    if (Edges[i].GetOtherNode(this) == node)
+                        return Edges[i];
+                }
+                var edge = new Edge(this, node);
+                Edges.Add(edge);
+                node.Edges.Add(edge);
+                return edge;
+            }
+
+            public void Disconnect(Node node)
+            {
+                if (null == node)
+                    return;
+                for (int i = 0; i < Edges.Count; ++i)
+                {
+                    if (Edges[i].GetOtherNode(this) == node)
+                    {
+                        node.Edges.Remove(Edges[i]);
+                        Edges.RemoveAt(i);
+                        return;
+                    }
+                }
             }
         }
 
         public class Edge
         {
-            private readonly int _sourceVertex;
-            private readonly int _targetVertex;
-            private readonly double _weight;
+            public Node Start { get; }
+            public Node End { get; }
+            public float Weight { get; set; }
 
-            public Edge(int sourceVertex, int targetVertex, double weight)
+            public Edge(Node start, Node end)
             {
-                _sourceVertex = sourceVertex;
-                _targetVertex = targetVertex;
-                _weight = weight;
+                Start = start;
+                End = end;
             }
 
-            public int SourceVertex()
+            public Node GetOtherNode(Node node)
             {
-                return _sourceVertex;
+                if (Start == node)
+                    return End;
+                if (End == node)
+                    return Start;
+                return null;
             }
 
-            // Can you compare two items like this?
-            public int TargetVertex(int vertex)
-            {
-                if (vertex == _sourceVertex)
-                {
-                    return _targetVertex;
-                }
-                if (vertex == _targetVertex)
-                {
-                    return _sourceVertex;
-                }
-                throw new Exception("Illegal endpoint.");
-            }
-
-            public double Weight()
-            {
-                return _weight;
-            }
-
-            // Compares two edges.
             // One edge is less than the other if its weight is less.
             public int CompareTo(Edge sourceEdge, Edge targetEdge)
             {
-                if (sourceEdge.Weight() < targetEdge.Weight())
+                if (sourceEdge.Weight < targetEdge.Weight)
                 {
                     return -1;
                 }
-                if (sourceEdge.Weight() > targetEdge.Weight())
+                if (sourceEdge.Weight > targetEdge.Weight)
                 {
                     return +1;
                 }
                 return 0;
             }
-
-            // String representation of the graph
-            public string GraphToString()
-            {
-                return string.Format("{0:d}-{1:d} {2:f5}", _sourceVertex.ID, _targetVertex.ID, _weight);
-            }
         }
 
-        public class Graph
+        readonly List<Node> _nodes = new List<Node>();
+
+        public Node Head { get; }
+
+        public Node CreateNode(DataType value)
         {
-            private int _numberOfVertices;
-            private int _numberOfEdges;
-            private LinkedList<Edge>[] _adjacencyList;  //Using a linked list for adjacency-list representation.
+            var node = new Node(value);
+            _nodes.Add(node);
+            return node;
+        }
 
-            // Verticies is the number (count) of items in _itemsMasterList 
-            public Graph(int Vertices)
+        public void DeleteNode(Node node)
+        {
+            if (null == node)
+                return;
+            int index = _nodes.IndexOf(node);
+            if (index < 0)
+                return;
+            var edges = new List<Edge>(node.Edges);
+            for (int i = 0; i < edges.Count; ++i)
             {
-                if (Vertices < 0)
-                {
-                    throw new Exception("Number of vertices must be non-negative");
-                }
-                _numberOfVertices = Vertices;
-                _numberOfEdges = 0;
-                _adjacencyList = new LinkedList<Edge>[Vertices];
-
-                for (int i = 0; i < Vertices; i++)
-                {
-                    _adjacencyList[i] = new LinkedList<Edge>();
-                }
+                Node other = edges[i].GetOtherNode(node);
+                other.Disconnect(node);
             }
-
-            public int NumberOfVerticies()
-            {
-                return _numberOfVertices;
-            }
-
-            public int NumberOfEdges()
-            {
-                return _numberOfEdges;
-            }
-
-            // Since this is an undirected graph, adding new edge to each vertice's adjacency list.
-            public void AddEdge(Edge edge)
-            {
-                int sourceVertex = edge.SourceVertex();
-                int targetVertex = edge.TargetVertex(sourceVertex);
-                _adjacencyList[sourceVertex].AddFirst(edge);
-                _adjacencyList[targetVertex].AddFirst(edge);
-                _numberOfEdges++;
-            }
-
-            // Return edges for a specific vertex
-            public IEnumerable<Edge> Adj(int vertex)
-            {
-                return _adjacencyList[vertex];
-            }
-
-            // TODO: Return all edges in the graph
+            _nodes.RemoveAt(index);
         }
     }
 }
